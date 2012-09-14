0a26b840cc7847118f4a3f81669ac938bf8fa3da
Tass Skoudros <tass@theappbusiness.com>
2012-09-14 18:29:00 +0100

    updated to use fcgid
    Signed-off-by: Tass Skoudros <tass@theappbusiness.com>

--- roles/lamp_server.rb
+++ roles/lamp_server.rb
@@ -1,4 +1,3 @@
 name "lamp_server"
 description "Setup a LAMP Server"
-run_list "role[base]", "recipe[apache2]", "recipe[openssl]", "recipe[mysql::server]", "recipe[mysql]", "recipe[php]", "recipe[skystack]"
-default_attributes "apache2" => { "default_modules" => [ "fcgid" ] }
+run_list "role[base]", "recipe[apache2]", "recipe[apache2::mod_fcgid]", "recipe[openssl]", "recipe[mysql::server]", "recipe[mysql]", "recipe[php]", "recipe[skystack]"
