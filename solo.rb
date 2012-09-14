e6ee006077042929f423cd492b201ccd40734dd7
Tass Skoudros <tass@theappbusiness.com>
2012-09-14 14:03:30 +0100

    updated fixed site-cookbooks
    Signed-off-by: Tass Skoudros <tass@theappbusiness.com>

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
-file_cache_path "/opt/skystack/local/kitchen/tmp"
+file_cache_path "/opt/skystack/tmp"
 data_bag_path   "/opt/skystack/local/kitchen/data_bags"
-cookbook_path   [ "/opt/skystack/local/site-cookbooks",
+cookbook_path   [ "/opt/skystack/local/kitchen/site-cookbooks",
                   "/opt/skystack/local/kitchen/cookbooks" ]
 role_path       "/opt/skystack/local/kitchen/roles"
