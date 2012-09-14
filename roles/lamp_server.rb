38a3fc36e0841708777be33882af23b9618ed614
Tass Skoudros <tass@theappbusiness.com>
2012-09-14 13:51:22 +0100

    updated to add fcgid for php
    Signed-off-by: Tass Skoudros <tass@theappbusiness.com>

--- roles/lamp_server.rb
+++ roles/lamp_server.rb
@@ -1,3 +1,4 @@
 name "lamp_server"
 description "Setup a LAMP Server"
 run_list "role[base]", "recipe[apache2]", "recipe[openssl]", "recipe[mysql::server]", "recipe[mysql]", "recipe[php]", "recipe[skystack]"
+default_attributes "apache2" => { "default_modules" => [ "fcgid" ] }
