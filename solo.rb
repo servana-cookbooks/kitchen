eb2f1020813ca29e0a255c2de21042eade5755da
Tass Skoudros <tass@theappbusiness.com>
2012-09-14 09:25:40 +0100

    updated paths for the kitchen in solo.rb
    Signed-off-by: Tass Skoudros <tass@theappbusiness.com>

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
 file_cache_path "/opt/skystack/local/kitchen/tmp"
 data_bag_path   "/opt/skystack/local/data_bags"
-cookbook_path   [ "opt/skystack/local/site-cookbooks",
-                  "opt/skystack/local/cookbooks" ]
-role_path       "opt/skystack/local/roles"
+cookbook_path   [ "/opt/skystack/local/site-cookbooks",
+                  "/opt/skystack/local/cookbooks" ]
+role_path       "/opt/skystack/local/roles"
